4a23acbd43b077a20ea7f0e2a524291cefb125d1
Youssef-MSF <youssefmsf123@gmail.com>
2022-09-16 00:17:44 +0200

    First commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+import sys
+
+# Get image path from execution command argument
+image_path = sys.argv[1]
+
+# Get classifier path
+classifier_path = sys.argv[2]
+
+faceCascade = cv2.CascadeClassifier(classifier_path)
+
+img = cv2.imread(image_path)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+faces = faceCascade.detectMultiScale(gray,scaleFactor=1.2,
+    minNeighbors = 5, minSize=(25,25))
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(gray,(x,y),(x+w,y+h),(255,0,0),2)
+    plate = gray[y: y+h, x:x+w]
+    plate = cv2.blur(plate,ksize=(20,20))
+    # put the blurred plate into the original image
+    gray[y: y+h, x:x+w] = plate
+
+cv2.imshow('plates',gray)
